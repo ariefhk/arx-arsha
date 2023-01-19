d3c66f9a3ba8e412160fe287e7006b18f0558ba1
ariefhk <ariefrachmanhakim1001@gmail.com>
2023-01-20 00:53:59 +0700

    first navbar component

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,8 +1,69 @@
 import React from "react";
-import { Navbar, Container, Nav, Image } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import "./NavbarComponent.css";
 
 function NavbarComponent() {
-  return <div>NavbarComponent</div>;
+  const navLink = [
+    {
+      name: "Home",
+      url: "/home",
+    },
+    {
+      name: "About",
+      url: "/about",
+    },
+    {
+      name: "Services",
+      url: "/services",
+    },
+    {
+      name: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      name: "Team",
+      url: "/team",
+    },
+    {
+      name: "Drop Down",
+      url: "/dropdown",
+    },
+    {
+      name: "Contact",
+      url: "/contact",
+    },
+  ];
+
+  return (
+    <Container id="navbar">
+      <Navbar expand="lg">
+        <Navbar.Brand className="navbar-logo" href="#home">
+          ARSHA
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navbar-nav ms-auto">
+            {navLink.map((link) => {
+              if (link.name === "Drop Down") {
+                return (
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Action
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Another action
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                );
+              }
+              return <Nav.Link href={link.url}>{link.name}</Nav.Link>;
+            })}
+          </Nav>
+        </Navbar.Collapse>
+        <Button className="navbar-getstarted">Get Started</Button>
+      </Navbar>
+    </Container>
+  );
 }
 
 export default NavbarComponent;
